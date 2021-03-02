0abde4b5533b3486eebaa6866e391c69c439172c
Claudio Torcato <claudio.torcato@trt22.jus.br>
2021-03-01 22:27:17 -0300

    Novos testes

--- lord/__init__.py
+++ lord/__init__.py
@@ -1,4 +1,6 @@
 import random
+from functools import reduce
+from collections import UserList
 
 class Card():
     def __init__(self, nome):
@@ -8,8 +10,17 @@ class Card():
     def virar(self):
         self.virado = True
 
+    def __repr__(self):
+        return self.nome
+
 class Hero(Card):
-    ...
+    def __init__(self, nome, custo_ameaça):
+        super().__init__(nome)
+        self.custo_ameaça = custo_ameaça
+        self.recursos = 0
+
+    def adicionar_recurso(self):
+        self.recursos += 1
 
 class Deck():
     def __init__(self):
@@ -34,12 +45,21 @@ class Deck():
         self.herois = h
         self.cartas = c
 
+class Mão(UserList):
+    def index(self, valor):
+        for count, value in enumerate(self.data):
+            if value.nome == valor:
+                return count
+        else:
+            raise ValueError
+
 class Jogador():
     def __init__(self, nome = 'Jogador 1'):
         self.nome = nome
-        self.hand = []
+        self.hand = Mão()
         self.espaço_herois = []
         self.deck = None
+        self.mesa = []
 
     def usar_deck(self, deck):
         self.deck = deck
@@ -47,6 +67,11 @@ class Jogador():
     @property
     def mão(self):
         return self.hand
+
+    @property
+    def ameaça(self):
+        return sum(h.custo_ameaça for h in self.espaço_herois)
+        #return reduce((lambda x, y: x.custo_ameaça + y.custo_ameaça), self.espaço_herois)
 
     def separar(self):
         self.deck.separar()
@@ -56,8 +81,30 @@ class Jogador():
         random.shuffle(self.deck.cartas)
 
     def comprar_mão_inicial(self):
-        self.hand = self.deck.cartas[0:6]
+        self.hand = Mão(self.deck.cartas[0:6])
         self.deck.cartas = self.deck.cartas[6:]
+
+    def comprar(self):
+        self.hand.extend(self.deck.cartas[0:1])
+        self.deck.cartas = self.deck.cartas[1:]
+
+    def adicionar_recursos(self):
+        for heroi in self.espaço_herois:
+            heroi.adicionar_recurso()
+
+    def jogar(self, nome_carta):
+        pos = self.hand.index(nome_carta)
+        print(pos)
+        carta = self.hand.pop(pos)
+        if not carta:
+            raise Exception
+        self.mesa.append( carta )
+
+    def pagar_de(self, heroi, quantidade):
+        for count, value in enumerate( self.espaço_herois):
+            if value.nome == heroi:
+                self.espaço_herois[count].recursos -= quantidade
+                break
 
 
 class Jogo():
--- test_model.py
+++ test_model.py
@@ -11,16 +11,16 @@ def jogo_com_um_jogador():
 @pytest.fixture
 def deck_duas_cartas():
     deck = lord.Deck()
-    deck.nova_carta(lord.Hero('Balin'))
+    deck.nova_carta(lord.Hero('Balin', 9))
     deck.nova_carta(lord.Card('The One Ring'))
     return deck
 
 @pytest.fixture
 def deck_para_abertura():
     deck = lord.Deck()
-    deck.nova_carta(lord.Hero('Elfhelm'))
-    deck.nova_carta(lord.Hero('Éomer'))
-    deck.nova_carta(lord.Hero('Éowyn'))
+    deck.nova_carta(lord.Hero('Elfhelm', 10))
+    deck.nova_carta(lord.Hero('Éomer', 10))
+    deck.nova_carta(lord.Hero('Éowyn', 9))
     deck.nova_carta(lord.Card('Snowbourn Scout'))
     deck.nova_carta(lord.Card('Windfola'))
     deck.nova_carta(lord.Card('Rohan Warhorse'))
@@ -71,3 +71,43 @@ def test_mão_inicial(jogo_com_um_jogador, deck_para_abertura):
     jogo.jogador1.comprar_mão_inicial()
     assert len(jogo.jogador1.espaço_herois) == 3
     assert len(jogo.jogador1.mão) == 6
+
+def test_ameaça_inicial_é_29(jogo_com_um_jogador, deck_para_abertura):
+    jogo = jogo_com_um_jogador
+    jogo.jogador1.usar_deck(deck_para_abertura)
+    jogo.jogador1.separar()
+    assert jogo.jogador1.ameaça == 29
+
+def test_total_recursos_pos_fase_recursos(jogo_com_um_jogador, deck_para_abertura):
+    jogo = jogo_com_um_jogador
+    jogo.jogador1.usar_deck(deck_para_abertura)
+    jogo.jogador1.separar()
+    jogo.jogador1.embaralhar_deck()
+    jogo.jogador1.comprar_mão_inicial()
+    jogo.jogador1.adicionar_recursos()
+    assert jogo.jogador1.espaço_herois[0].recursos == 1
+    assert jogo.jogador1.espaço_herois[1].recursos == 1
+    assert jogo.jogador1.espaço_herois[2].recursos == 1
+
+def test_comprar_carta(jogo_com_um_jogador, deck_para_abertura):
+    jogo = jogo_com_um_jogador
+    jogo.jogador1.usar_deck(deck_para_abertura)
+    jogo.jogador1.separar()
+    jogo.jogador1.embaralhar_deck()
+    jogo.jogador1.comprar_mão_inicial()
+    jogo.jogador1.comprar()
+    assert len(jogo.jogador1.mão) == 7
+
+def test_jogar_carta_custo_1(jogo_com_um_jogador, deck_para_abertura):
+    jogo = jogo_com_um_jogador
+    jogo.jogador1.usar_deck(deck_para_abertura)
+    jogo.jogador1.separar()
+    jogo.jogador1.comprar_mão_inicial()
+    jogo.jogador1.adicionar_recursos()
+    jogo.jogador1.comprar()
+    jogo.jogador1.jogar('Snowbourn Scout')
+    jogo.jogador1.pagar_de('Elfhelm', 1)
+    assert len(jogo.jogador1.mão) == 6
+    assert len(jogo.jogador1.mesa) == 1
+    assert jogo.jogador1.mesa[0].virado == False
+    assert jogo.jogador1.espaço_herois[0].recursos == 0
